227d92d51c3c30baf3d3d8e7da51c689175e0c72
dantmac <dantmac@mail.ru>
2021-05-26 20:10:56 +0300

    added network services

---
+++ VKNewsFeed/Services/API.swift
@@ -0,0 +1,16 @@
+//
+//  API.swift
+//  VKNewsFeed
+//
+//  Created by admin on 26.05.2021.
+//
+
+import Foundation
+
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.131"
+    
+    static let newsFeed = "/method/newsfeed.get"
+}
